2003034d971cd8d6627918ce3282d1be5aeeee58
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-06-07 19:42:09 +0700

    add promoLabel, price, rating, sold, to ProductCard

--- finished/modules/common/components/ProductCard/index.js
+++ finished/modules/common/components/ProductCard/index.js
@@ -1,39 +1,80 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
+import PromoLabel from '@common/components/PromoLabel';
+import ProductRating from '@common/components/ProductRating';
+
+import { currencyFormatter } from '@utils/currency';
+
 const useStyles = makeStyles({
-  root: {
+  card: {
     maxWidth: 360,
   },
   image: {
     height: 140,
+  },
+  cardContent: {
+    padding: 8,
+    paddingTop: 16,
+  },
+  relative: {
+    position: 'relative'
+  },
+  promoLabel: {
+    position: 'absolute',
+    bottom: 6,
+    left: 4,
+  },
+  price: {
+    lineHeight: 'normal',
   }
 });
 
-const ProductCard = () => {
+const ProductCard = ({ img, title, promoLabel, price, rating, sold }) => {
   const classes = useStyles();
+  console.log('rating', rating)
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia 
+    <Card className={classes.card}>
+      <div className={classes.relative}>
+        <CardMedia
           className={classes.image}
-          image="/images/asparagus-on-white.jpg"
-          title="Asparagus"
+          image={img}
+          title={title}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Asparagus
+        <div className={classes.promoLabel}>
+          <PromoLabel promoLabel={promoLabel} />
+        </div>
+      </div>
+      <CardContent className={classes.cardContent}>
+        <Grid container direction="column">
+          <Typography gutterBottom variant="subtitle2" component="h2">
+            {title}
           </Typography>
-        </CardContent>
-      </CardActionArea>
+          <Typography variant="overline" className={classes.price}>
+            {currencyFormatter(price)}
+          </Typography>
+          <ProductRating rating={rating} sold={sold} />
+        </Grid>
+      </CardContent>
     </Card>
   );
+}
+
+ProductCard.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  promoLabel: PropTypes.array,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number,
+  sold: PropTypes.sold,
 }
 
 export default ProductCard;
--- finished/pages/index.js
+++ finished/pages/index.js
@@ -5,22 +5,43 @@ import Grid from '@material-ui/core/Grid';
 import Layout from '@common/components/Layout';
 import ProductCard from '@common/components/ProductCard';
 
-const HomePage = () => {
+const HomePage = ({ products }) => {
+  console.log("products", products);
+  const { list } = products;
   return (
     <Layout>
       <Container maxWidth="sm">
-        <div>This is inside a container</div>
         <Grid container spacing={2}>
-          <Grid item xs={6} sm={6}>
-            <ProductCard />
-          </Grid>
-          <Grid item xs={6} sm={6}>
-            <ProductCard />
-          </Grid>
+          {list.map(list => {
+            // console.log(list)
+            return (
+              <Grid key={list.id} item xs={6} sm={6}>
+                <ProductCard
+                  img={list.img}
+                  title={list.name}
+                  promoLabel={list.promo}
+                  price={list.price}
+                  rating={list.rating}
+                  sold={list.sold}
+                />
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:5000/products');
+  const products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+  }
 }
 
 export default HomePage;
